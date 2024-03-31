f70d3918edb6e13ad14d092808dbb04515b2e923
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2024-03-31 12:42:55 +0000

    Adding report files and updated analysis

--- commands/analyse.mjs
+++ commands/analyse.mjs
@@ -1,7 +1,8 @@
 import {getDirStructure} from "../directoryProcessor.mjs"
-import {inferDependency, inferFileImports, inferProjectDirectory} from "../openai.mjs"
+import {calculateTokens, inferDependency, inferFileImports, inferProjectDirectory} from "../openai.mjs"
 import {parseTree} from "../treeParser.mjs"
 import fs from 'fs'
+import {addAnalysisInGitIgnore, writeAnalysis} from "../utils.mjs"
 export const command = 'analyse <path|p> [verbose|v] [openai|o] [streaming|s]'
 
 export const describe = 'Analyse the given directory structure to understand the project structure and dependencies'
@@ -45,12 +46,35 @@ export async function handler(argv) {
     if (isVerbose) {
         console.log(`Analyse the given directory structure to understand the project structure and dependencies: ${argv.path}`)
     }
-    const directoryStructure = await getDirStructure(argv.path, isVerbose)
+    const projectName = argv.path
+    addAnalysisInGitIgnore(projectName)
+    console.log("Reading codebase structure and file...")
+    const directoryStructureWithContent = await getDirStructure(argv.path, isVerbose)
+
+    const directoryStructure = JSON.parse(JSON.stringify(directoryStructureWithContent))
+
+    function deleteContent(file) {
+        delete file.content
+        if (file.children) {
+            for (const child of file.children) {
+                deleteContent(child)
+            }
+        }
+    }
+
+    for (const file of directoryStructure.children) {
+        deleteContent(file)
+    }
+
+
+    writeAnalysis(projectName, "directoryStructure", directoryStructure, true)
+    writeAnalysis(projectName, "directoryStructureWithFileContent", directoryStructureWithContent, true)
     const directoryInferrenceResponse = await inferProjectDirectory(directoryStructure, useOpenAi, allowStreaming, isVerbose)
     const directoryInferrence = JSON.parse(directoryInferrenceResponse ?? "")
+
+    writeAnalysis(projectName, "directoryInferrence", directoryInferrence, true)
     // const message =
-    console.log(directoryInferrence)
-    const projectName = argv.path
+    console.log("Analyzed the directory structure...")
 
     if (isVerbose) {
         console.log({project: argv.path, directoryInferrence})
@@ -62,24 +86,48 @@ export async function handler(argv) {
 
     const directories = []
     if (!directoryInferrence.isMonorepo) {
+        console.log("Reading dependency file...")
         const depenencyFile = fs.readFileSync(`${argv.path}/${directoryInferrence.dependenciesFile}`, 'utf-8')
+        const lockfile = directoryInferrence.lockFile
+        const directoryStructureWithoutLockFile = JSON.parse(JSON.stringify(directoryStructureWithContent))
+        removeLockFile(directoryStructureWithoutLockFile, lockfile)
+
         const dependencyInferrence = await inferDependency(depenencyFile, directoryInferrence.workflow, useOpenAi, allowStreaming, isVerbose)
+        let dependencyInferrenceResponse = ""
         if (allowStreaming) {
+
             for await (const chunk of dependencyInferrence) {
                 console.log(chunk)
+                dependencyInferrenceResponse += chunk.toString()
             }
+
         } else {
             console.log(dependencyInferrence)
+            dependencyInferrenceResponse = dependencyInferrence
         }
-        const tree = await parseTree(`${argv.path}/${directoryInferrence.entryPointFile}`, directoryInferrence.treeSitterLanguage, isVerbose)
-        console.log(tree)
-        const fileImport = await inferFileImports(tree, useOpenAi, allowStreaming, isVerbose)
-        if (allowStreaming) {
-            for await (const chunk of fileImport) {
-                console.log(chunk)
-            }
+        writeAnalysis(projectName, "dependencyInferrence", dependencyInferrenceResponse)
+
+        if (isVerbose) {
+            console.log(`Directory Structure With file content: ${JSON.stringify(directoryStructureWithoutLockFile)}`)
+        }
+        const tokenLength = await calculateTokens([{content: JSON.stringify(directoryStructureWithoutLockFile), role: 'user'}])
+        if (isVerbose) {
+            console.log(`Token length of entire codebase: ${tokenLength}`)
+        }
+        if (tokenLength <= 128000) {
+            console.log("Codebase is small, can be inferred for a larger language model")
         } else {
-            console.log(fileImport)
+
+            const tree = await parseTree(`${argv.path}/${directoryInferrence.entryPointFile}`, directoryInferrence.treeSitterLanguage, isVerbose)
+            console.log(tree)
+            const fileImport = await inferFileImports(tree, useOpenAi, allowStreaming, isVerbose)
+            if (allowStreaming) {
+                for await (const chunk of fileImport) {
+                    console.log(chunk)
+                }
+            } else {
+                console.log(fileImport)
+            }
         }
     } else {
         directories.push(...directoryInferrence.directories)
@@ -90,6 +138,19 @@ export async function handler(argv) {
 
 
 }
+// Find the lockfile recursively and remove it from entry
+function removeLockFile(file, lockfile) {
+    if (file.children) {
+        for (const child of file.children) {
+            if (child.name === lockfile) {
+                file.children = file.children.filter(child => child.name !== lockfile)
+            } else {
+                removeLockFile(child)
+            }
+        }
+    }
+}
+
 export const usage = '$0 <cmd> [args]'
 
 export const aliases = ['h', 'help']
--- utils.mjs
+++ utils.mjs
@@ -11,3 +11,29 @@ export function readConfig() {
     }
     return {}
 }
+
+export function addAnalysisInGitIgnore(projectRoot) {
+    // First check if the project root has gitignore file, if the file is not found don't create one.
+    // If the file is found, append .SourceSailor/analysis to the file.
+    const gitignoreFile = path.join(projectRoot, '.gitignore')
+    if (fs.existsSync(gitignoreFile)) {
+        const gitignoreContent = fs.readFileSync(gitignoreFile, 'utf8')
+        if (!gitignoreContent.includes('.SourceSailor/analysis')) {
+            fs.appendFileSync(gitignoreFile, '\n.SourceSailor/analysis')
+        }
+    }
+}
+
+export function writeAnalysis(projectRoot, analysisName, analysisContent, isJson = false) {
+    const analysisDir = path.join(projectRoot, '.SourceSailor', 'analysis')
+    if (!fs.existsSync(analysisDir)) {
+        fs.mkdirSync(analysisDir, {recursive: true})
+    }
+    if (isJson) {
+        const analysisFile = path.join(analysisDir, `${analysisName}.json`)
+        fs.writeFileSync(analysisFile, JSON.stringify(analysisContent, null, 4))
+    } else {
+        const analysisFile = path.join(analysisDir, `${analysisName}.md`)
+        fs.writeFileSync(analysisFile, analysisContent)
+    }
+}
