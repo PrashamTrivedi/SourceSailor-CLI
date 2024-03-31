43584b1e5d89fb15134c40ae46411c4cdee8ca1c
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2024-03-31 14:46:19 +0000

    Started working with Monorepos

--- commands/analyse.mjs
+++ commands/analyse.mjs
@@ -51,7 +51,56 @@ export async function handler(argv) {
     const projectName = argv.path
     addAnalysisInGitIgnore(projectName)
     console.log("Reading codebase structure and file...")
-    const directoryStructureWithContent = await getDirStructure(argv.path, isVerbose)
+    const path = argv.path
+    const isRoot = true
+    const {directoryInferrence, directoryStructureWithContent} = await analyseDirectoryStructure(path, isVerbose, isRoot, projectName, useOpenAi)
+
+    if (isVerbose) {
+        console.log({project: argv.path, directoryInferrence})
+    }
+
+
+
+    console.log(`Analysing ${projectName}'s file structure to getting started.`)
+
+    if (!directoryInferrence.isMonorepo) {
+        const sourceCodePath = argv.path
+        await inferDependenciesAndWriteAnalysis(sourceCodePath, directoryInferrence, useOpenAi, allowStreaming, isVerbose, projectName)
+
+        const {tokenLength, directoryStructureWithoutLockFile} = await calculateCodebaseTokens(directoryInferrence, directoryStructureWithContent, isVerbose)
+        await analyzeCode(tokenLength, directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose, projectName)
+    } else {
+
+        if (isVerbose) {
+            console.log({directories: directoryInferrence.directories})
+        }
+
+        for await (const directory of directoryInferrence.directories) {
+            try {
+                console.log(`Analysing ${directory}'s file structure to getting started.`)
+                const sourceCodePath = `${argv.path}/${directory}`
+                const {directoryInferrence, directoryStructureWithContent} = await analyseDirectoryStructure(sourceCodePath, isVerbose, false, projectName, useOpenAi)
+
+                await inferDependenciesAndWriteAnalysis(sourceCodePath, directoryInferrence, useOpenAi, allowStreaming, isVerbose, projectName)
+
+                const {tokenLength, directoryStructureWithoutLockFile} = await calculateCodebaseTokens(directory, directoryStructureWithContent, isVerbose)
+                await analyzeCode(tokenLength, directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose, projectName)
+            } catch (error) {
+                console.error(`Error analysing ${directory}: Moving on to next directory...`)
+                if (isVerbose) {
+                    console.error(error)
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
+async function analyseDirectoryStructure(path, isVerbose, isRoot, projectName, useOpenAi) {
+    const directoryStructureWithContent = await getDirStructure(path, isVerbose)
 
     const directoryStructure = JSON.parse(JSON.stringify(directoryStructureWithContent))
 
@@ -69,112 +118,120 @@ export async function handler(argv) {
     }
 
 
-    writeAnalysis(projectName, "directoryStructure", directoryStructure, true)
-    writeAnalysis(projectName, "directoryStructureWithFileContent", directoryStructureWithContent, true)
+    if (isRoot) {
+        writeAnalysis(projectName, "directoryStructure", directoryStructure, true)
+        writeAnalysis(projectName, "directoryStructureWithFileContent", directoryStructureWithContent, true)
+    }
     const directoryInferrenceResponse = await inferProjectDirectory(directoryStructure, useOpenAi, false, isVerbose)
     const directoryInferrence = JSON.parse(directoryInferrenceResponse ?? "")
 
     writeAnalysis(projectName, "directoryInferrence", directoryInferrence, true)
     // const message =
     console.log("Analyzed the directory structure...")
-
-    if (isVerbose) {
-        console.log({project: argv.path, directoryInferrence})
-    }
-
-
-
-    console.log(`Analysing ${projectName}'s file structure to getting started.`)
-
-    const directories = []
-    if (!directoryInferrence.isMonorepo) {
-        console.log("Reading dependency file...")
-        const depenencyFile = fs.readFileSync(`${argv.path}/${directoryInferrence.dependenciesFile}`, 'utf-8')
-        const lockfile = directoryInferrence.lockFile
-        const directoryStructureWithoutLockFile = JSON.parse(JSON.stringify(directoryStructureWithContent))
-        removeLockFile(directoryStructureWithoutLockFile, lockfile)
-
-        const dependencyInferrence = await inferDependency(depenencyFile, directoryInferrence.workflow, useOpenAi, allowStreaming, isVerbose)
-        let dependencyInferrenceResponse = ""
-        if (allowStreaming) {
-
-            for await (const chunk of dependencyInferrence) {
-                const message = chunk.choices[0]?.delta.content || ""
-                process.stdout.write(message)
-                dependencyInferrenceResponse += message
-            }
-            process.stdout.write("\n")
-
-        } else {
-            console.log(dependencyInferrence)
-            dependencyInferrenceResponse = dependencyInferrence
-        }
-        writeAnalysis(projectName, "dependencyInferrence", dependencyInferrenceResponse)
-
-        if (isVerbose) {
-            console.log(`Directory Structure With file content: ${JSON.stringify(directoryStructureWithoutLockFile)}`)
-        }
-        const tokenLength = await calculateTokens([{content: JSON.stringify(directoryStructureWithoutLockFile), role: 'user'}])
-        if (isVerbose) {
-            console.log(`Token length of entire codebase: ${tokenLength}`)
-        }
-        if (tokenLength <= 128000) {
-            console.log("Reading Codebase and inferring code..")
-            const codeInferrence = await inferCode(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose)
-            let codeInferrenceResponse = ""
-            if (allowStreaming) {
-                for await (const chunk of codeInferrence) {
-                    const message = chunk.choices[0]?.delta.content || ""
-                    process.stdout.write(message)
-                    codeInferrenceResponse += message
-                }
-                process.stdout.write("\n")
-            } else {
-                console.log(codeInferrence)
-                codeInferrenceResponse = codeInferrence
-            }
-            console.log("Getting some interesting parts of code..")
-            const interestingCode = await inferInterestingCode(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose)
-            let interestingCodeResponse = ""
-            if (allowStreaming) {
-                for await (const chunk of interestingCode) {
-                    const message = chunk.choices[0]?.delta.content || ""
-                    process.stdout.write(message)
-                    interestingCodeResponse += message
-                }
-                process.stdout.write("\n")
-            } else {
-                console.log(interestingCode)
-                interestingCodeResponse = interestingCode
-            }
-            // Concatenate the code inferrence and interesting code
-            codeInferrenceResponse += interestingCodeResponse
-            writeAnalysis(projectName, "codeInferrence", codeInferrenceResponse)
-        } else {
-
-            const tree = await parseTree(`${argv.path}/${directoryInferrence.entryPointFile}`, directoryInferrence.treeSitterLanguage, isVerbose)
-            console.log(tree)
-            const fileImport = await inferFileImports(tree, useOpenAi, allowStreaming, isVerbose)
-            if (allowStreaming) {
-                for await (const chunk of fileImport) {
-                    const message = chunk.choices[0]?.delta.content || ""
-                    process.stdout.write(message)
-
-                }
-                process.stdout.write("\n")
-            } else {
-                console.log(fileImport)
-            }
-        }
-    } else {
-        directories.push(...directoryInferrence.directories)
-    }
-
-
-
-
-
+    return {directoryInferrence, directoryStructureWithContent}
 }
+
+async function analyzeCode(tokenLength, directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose, projectName) {
+    if (tokenLength <= 128000) {
+        await analyzeAndWriteCodeInference(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose, projectName)
+    } else {
+
+        // const tree = await parseTree(`${argv.path}/${directoryInferrence.entryPointFile}`, directoryInferrence.treeSitterLanguage, isVerbose)
+        // console.log(tree)
+        // const fileImport = await inferFileImports(tree, useOpenAi, allowStreaming, isVerbose)
+        // if (allowStreaming) {
+        //     for await (const chunk of fileImport) {
+        //         const message = chunk.choices[0]?.delta.content || ""
+        //         process.stdout.write(message)
+        //     }
+        //     process.stdout.write("\n")
+        // } else {
+        //     console.log(fileImport)
+        // }
+        console.log("This codebase is too big, but hold tight!! We are working on it.")
+    }
+}
+
+async function analyzeAndWriteCodeInference(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose, projectName) {
+    let codeInferrenceResponse = await analyzeCodebase(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose)
+    console.log("Getting some interesting parts of code..")
+    let interestingCodeResponse = await analyseInterestingCode(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose)
+    // Concatenate the code inferrence and interesting code
+    codeInferrenceResponse += interestingCodeResponse
+    writeAnalysis(projectName, "codeInferrence", codeInferrenceResponse)
+}
+
+async function analyseInterestingCode(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose) {
+    const interestingCode = await inferInterestingCode(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose)
+    let interestingCodeResponse = ""
+    if (allowStreaming) {
+        for await (const chunk of interestingCode) {
+            const message = chunk.choices[0]?.delta.content || ""
+            process.stdout.write(message)
+            interestingCodeResponse += message
+        }
+        process.stdout.write("\n")
+    } else {
+        console.log(interestingCode)
+        interestingCodeResponse = interestingCode
+    }
+    return interestingCodeResponse
+}
+
+async function analyzeCodebase(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose) {
+    console.log("Reading Codebase and inferring code..")
+    const codeInferrence = await inferCode(directoryStructureWithoutLockFile, useOpenAi, allowStreaming, isVerbose)
+    let codeInferrenceResponse = ""
+    if (allowStreaming) {
+        for await (const chunk of codeInferrence) {
+            const message = chunk.choices[0]?.delta.content || ""
+            process.stdout.write(message)
+            codeInferrenceResponse += message
+        }
+        process.stdout.write("\n")
+    } else {
+        console.log(codeInferrence)
+        codeInferrenceResponse = codeInferrence
+    }
+    return codeInferrenceResponse
+}
+
+async function calculateCodebaseTokens(directoryInferrence, directoryStructureWithContent, isVerbose) {
+    const lockfile = directoryInferrence.lockFile
+    const directoryStructureWithoutLockFile = JSON.parse(JSON.stringify(directoryStructureWithContent))
+    removeLockFile(directoryStructureWithoutLockFile, lockfile)
+
+
+    const tokenLength = await calculateTokens([{content: JSON.stringify(directoryStructureWithoutLockFile), role: 'user'}])
+    if (isVerbose) {
+        console.log(`Token length of entire codebase: ${tokenLength}`)
+    }
+    return {tokenLength, directoryStructureWithoutLockFile}
+}
+
+async function inferDependenciesAndWriteAnalysis(sourceCodePath, directoryInferrence, useOpenAi, allowStreaming, isVerbose, projectName) {
+    console.log("Reading dependency file...")
+    const depenencyFile = fs.readFileSync(`${sourceCodePath}/${directoryInferrence.dependenciesFile}`, 'utf-8')
+    const dependencyInferrence = await inferDependency(depenencyFile, directoryInferrence.workflow, useOpenAi, allowStreaming, isVerbose)
+
+
+    let dependencyInferrenceResponse = ""
+    if (allowStreaming) {
+
+        for await (const chunk of dependencyInferrence) {
+            const message = chunk.choices[0]?.delta.content || ""
+            process.stdout.write(message)
+            dependencyInferrenceResponse += message
+        }
+        process.stdout.write("\n")
+
+    } else {
+        console.log(dependencyInferrence)
+        dependencyInferrenceResponse = dependencyInferrence
+    }
+    writeAnalysis(projectName, "dependencyInferrence", dependencyInferrenceResponse)
+}
+
 // Find the lockfile recursively and remove it from entry
 function removeLockFile(file, lockfile) {
     if (file.children) {
@@ -187,6 +244,7 @@ function removeLockFile(file, lockfile) {
         }
     }
 }
+
 
 export const usage = '$0 <cmd> [args]'
 
