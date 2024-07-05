324975fc0980febf49aa136889d059149cb90448
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2024-07-05 14:01:20 +0000

    Updating coverage.

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
         root: '.', // Set this to your project root
         coverage: {
             provider: 'istanbul',
-            reporter: ['json', 'html', 'lcov'],
+            reporter: ['json', 'json-summary', 'text-summary', 'html'],
             // reportsDirectory: './coverage',
 
             include: ['**/*.mts', '**/openai.mts', '**/utils.mts', '**/*.mjs'], // Adjust this to match your source files
