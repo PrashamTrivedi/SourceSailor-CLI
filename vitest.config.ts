42ee15a1da430d870490617ed92f0aea71c8ae6e
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2024-07-05 13:32:06 +0000

    Adding some lcov into mix

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,15 +6,16 @@ export default defineConfig({
         root: '.', // Set this to your project root
         coverage: {
             provider: 'istanbul',
-            reporter: ['text', 'json', 'html'],
-            reportsDirectory: './coverage',
+            reporter: ['json', 'html', 'lcov'],
+            // reportsDirectory: './coverage',
+
             include: ['**/*.mts', '**/openai.mts', '**/utils.mts', '**/*.mjs'], // Adjust this to match your source files
             exclude: ['node_modules', 'test', '**/*.d.ts'],
         },
         include: ['**/*.{test,spec}.mts'],
         reporters: process.env.GITHUB_ACTIONS ? ['json', 'html', 'github-actions'] : ['default', 'json', 'html'],
         outputFile: {
-            json: './coverage/coverage-summary.json',
+            json: './coverage/test-summary.json',
             html: './coverage/report.html'
         }
     },
